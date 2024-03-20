831308513cffa70f161a67b2afbafc61176c1c7b
nagabhavya2020 <164063513+nagabhavya2020@users.noreply.github.com>
2024-03-20 13:36:58 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({baseURL : `http://localhost:5500/`})
+const API = axios.create({baseURL : `http://youtubeclone-mocha.vercel.app/`})
 
 API.interceptors.request.use(req=>{
     if(localStorage.getItem('Profile')){
